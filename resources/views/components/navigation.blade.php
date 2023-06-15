2aa6a5dc1e27341e41ac300319cbc1116c7a719a
marek-miklusek <marek.miklusek@gmail.com>
2023-06-15 14:40:13 +0000

    modify css in movies-tv component

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,6 +1,6 @@
 <div x-data="{ scrollDown: false }" x-init="window.addEventListener('scroll', () => scrollDown = window.pageYOffset > 50)" 
     class="w-full fixed top-0 z-50 text-white" :class="{ 'hidden': scrollDown }">
-    <div class="flex flex-wrap items-center px-3 md:px-16 py-6 justify-center">
+    <div class="flex flex-wrap items-center  px-3 md:px-16 py-6 justify-center">
 
         <div>
             <a href="/home">
