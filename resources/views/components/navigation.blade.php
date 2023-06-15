3f92ed23af14494bc1407178a09b6afda046fa18
marek-miklusek <marek.miklusek@gmail.com>
2023-06-15 13:18:36 +0000

    still problem with px in navigation

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,6 +1,6 @@
 <div x-data="{ scrollDown: false }" x-init="window.addEventListener('scroll', () => scrollDown = window.pageYOffset > 50)" 
-    class="w-full fixed top-0 z-50 text-white" :class="{ 'hidden': scrollDown }">
-    <div class="flex flex-wrap items-center px-3 sm:px-16 py-4 justify-center">
+    class="w-full fixed top-0 z-50 text-white px-3 sm:px-16" :class="{ 'hidden': scrollDown }">
+    <div class="flex flex-wrap items-center py-4 justify-center">
 
         <div>
             <a href="/home">
