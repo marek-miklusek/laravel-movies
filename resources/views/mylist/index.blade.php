fa54508c56c3bcc3e6a977de6e39fc4d311efa12
marek-miklusek <marek.miklusek@gmail.com>
2023-06-15 05:59:48 +0000

    change rate icon in my-list.index

--- resources/views/mylist/index.blade.php
+++ resources/views/mylist/index.blade.php
@@ -49,10 +49,10 @@
 
                                 {{-- Rate it --}}
                                 <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" @popper(Rate it!) 
-                                    class="bg-black w-9 h-9 border-2 border-white hover:border-[#21d07a] 
+                                    class="bg-black w-9 h-9 hover:border-[#21d07a] 
                                         ml-2 rounded-full flex justify-center items-center">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" 
-                                        stroke-width="1.5" stroke="#FFD700" class="w-5 h-5">
+                                        stroke-width="1.5" stroke="#FFD700" class="w-5 h-5 rate-svg">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 
                                         0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
                                     </svg>
