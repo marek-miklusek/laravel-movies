3df2a9f9f39bc37ef6e30a7505cde990dea703dd
marek-miklusek <marek.miklusek@gmail.com>
2023-06-17 13:21:28 +0000

    modify css in home-page

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,6 +1,6 @@
 <x-master-layout> 
     <div class="bg-black bg-opacity-60 py-16">
-        <div class="max-w-xl mt-16 mx-auto sm:px-6 lg:px-8 space-y-12">
+        <div class="max-w-xl mt-16 mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="bg-black bg-opacity-50 p-4 sm:p-8 sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-profile-information-form')
