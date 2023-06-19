eb35227f6313c743e28f1adb6f945e53e5f87e84
marek-miklusek <marek.miklusek@gmail.com>
2023-06-19 05:28:28 +0000

    change url in progress-bar

--- resources/views/components/progress-bar.blade.php
+++ resources/views/components/progress-bar.blade.php
@@ -3,7 +3,7 @@
 </div>
 
 <div class="fixed z-30 flex flex-col items-center space-y-4 bottom-5 right-5">
-    <a href="https://mikijunior.com" target="_blank" class="transition-transform transform hover:scale-125">
+    <a href="https://mikijunior.eu" target="_blank" class="transition-transform transform hover:scale-125">
         <svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" class="w-7 h-7 hover:opacity-100 opacity-50"><g id="SVGRepo_bgCarrier" stroke-width="1"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path fill="#fff" d="M628.736 528.896A416 416 0 0 1 928 928H96a415.872 415.872 0 0 1 299.264-399.104L512 704l116.736-175.104zM720 304a208 208 0 1 1-416 0 208 208 0 0 1 416 0z"></path></g></svg>
     </a>
     <a href="https://www.facebook.com/marek.miklusek" target="_blank" class="transition-transform transform hover:scale-125">
